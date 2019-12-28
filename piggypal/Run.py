2933f3bdf92dbb6c7d9a17d0ad4501f62550a332
Jannis Blossey <jannis@blossey.eu>
2019-12-28 18:22:08 +0100

    add piggypal as new application to Run.py

--- piggypal/Run.py
+++ piggypal/Run.py
@@ -1,9 +1,18 @@
 from Controllers.example import exampleController
 import cherrypy
 from cherrypy import tools
+import piggypal.Models.paypal_api_stub as piggypal
 
 def start_server():
     cherrypy.tree.mount(exampleController(), '/', 'Configs/example.conf')
+    cherrypy.tree.mount(piggypal.Paypal_stub(), '/piggypal', {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            #'tools.sessions.on': True,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        }
+    })
     cherrypy.config.update('Server.conf')
     cherrypy.engine.start()
     #cherrypy.engine.block()

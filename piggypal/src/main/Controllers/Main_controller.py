f8ab7e7a058bcc0267112ec8b61afdb495680c0e
jblossey <jannis@blossey.eu>
2020-01-07 17:59:24 +0100

    fix: piggypal button now reachable again
    ( Sign in Button [ch26] )

--- piggypal/src/main/Controllers/Main_controller.py
+++ piggypal/src/main/Controllers/Main_controller.py
@@ -1,7 +1,1 @@
-import cherrypy
-
-#deprecated/ moved to view
-class Piggypal_controller(object):
-    @cherrypy.expose
-    def GET(self):
-        return open('Views/login.html')
+#This Controller empty. Thus yeeted. 
--- piggypal/src/main/Run.py
+++ piggypal/src/main/Run.py
@@ -1,17 +1,13 @@
 import cherrypy
-from Controllers.Main_controller import Piggypal_controller
 from Views.Login_button import Login_button
-
-from cherrypy import tools
 from Models.Paypal_api_stub import Paypal_stub, Paypal_cred_listener
 
 
 
 def start_server():
-    cherrypy.tree.mount(Piggypal_controller(), '/', '')
+    cherrypy.tree.mount(Login_button(), '/', '')
     cherrypy.tree.mount(Paypal_stub(), '/piggypal', 'Configs/piggypal.conf')
     cherrypy.tree.mount(Paypal_cred_listener(), '/piggypal-listens', 'Configs/piggypal.conf')
-    cherrypy.tree.mount(Login_button(), '/piggypal-login', 'Configs/piggypal.conf')
     cherrypy.config.update('Configs/Server.conf')
     cherrypy.engine.start()
     #cherrypy.engine.block()

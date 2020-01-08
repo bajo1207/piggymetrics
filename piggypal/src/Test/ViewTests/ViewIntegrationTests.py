83f94f6261c4f3914a707a1fd8408fff93b57434
Andreas Spateneder <spateneder@campus.tu-berlin.de>
2020-01-08 23:37:11 +0100

    added View Tests

--- piggypal/src/Test/ViewTests/ViewIntegrationTests.py
+++ piggypal/src/Test/ViewTests/ViewIntegrationTests.py
@@ -1,30 +1,55 @@
 import cherrypy, sys, os
 from cherrypy.test import helper
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/../.."))
-from main.Views.Login_button import Login_button as lb
+from src.main.Views.Login_button import Login_button as lb
+from contextlib import contextmanager
+import requests
 
 
+@contextmanager
+def run_server():
+    cherrypy.config.update({'global': {'server.socket_host': "127.0.0.1", 'server.socket_port': 4710}})
+    cherrypy.tree.mount(lb(), "/", "")
+    cherrypy.engine.start()
+    cherrypy.engine.wait(cherrypy.engine.states.STARTED)
+    yield
+    cherrypy.engine.exit()
+
+
+@staticmethod
+def _setup():
+    cherrypy.tree.mount(lb(), "/", "")
+    cherrypy.config.update('Configs/Server.conf')
 
 
 class ViewIntegrationTests(helper.CPWebCase):
-    @staticmethod
-    def _setup():
-        cherrypy.tree.mount(lb(), "/", "")
-        cherrypy.config.update('Configs/Server.conf')
 
     def test_index(self):
         """
         Fails if request on "/" errors
         """
-        self.getPage("http://127.0.0.1:4710/")
-        self.assertStatus('200 OK')
+
+        with run_server():
+            url = "http://127.0.0.1:4710/"
+            r = requests.get(url)
+            print(r.status_code)
+            self.assertEqual(r.status_code, 200)
 
     def test_button(self):
         """
         Fails if returned object varys != Paypal Login button
         """
         url = "http://127.0.0.1:4710/"
-        self.getPage(url, method='GET')
-        f = open("Views/login.html", "r")
-        self.assertBody(f)
-
+        with run_server():
+            r = requests.get(url)
+            print(r.content)
+            f = open("PiggyPal Button.html", "r")
+            html = r.text
+            content = f.read()
+            html = html.replace(" ", "")
+            html = html.replace("\n", "")
+            content = content.replace(" ", "")
+            content = content.replace("\n", "")
+            r.close()
+            f.close()
+            self.assertEqual(html, content)

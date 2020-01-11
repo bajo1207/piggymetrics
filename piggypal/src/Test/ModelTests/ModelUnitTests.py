7731a70b715bf26040f33c3bdeea016083d3371e
Andreas Spateneder <spateneder@campus.tu-berlin.de>
2020-01-11 14:40:26 +0100

    added missing auth_code to Model Tests

--- piggypal/src/Test/ModelTests/ModelUnitTests.py
+++ piggypal/src/Test/ModelTests/ModelUnitTests.py
@@ -6,7 +6,7 @@ from contextlib import contextmanager
 from main.Models.Paypal_api_stub import Paypal_cred_listener as pcl, Paypal_stub as ps
 from utils import SRC_DIR
 
-conf = os.path.join(os.path.join(os.path.join(SRC_DIR, "main"), "Configs"), "Server.conf")
+conf = os.path.join(os.path.join(os.path.join(SRC_DIR, "main"), "Configs"), "piggypal.conf")
 
 
 @contextmanager
@@ -25,7 +25,7 @@ def run_server():
 
 class ModelUnitTests(helper.CPWebCase):
     """
-    Contains Tests for Model
+    Contains Unit Tests for Model
     """
     def test_credential_listener_get(self):
         """
@@ -68,8 +68,9 @@ class ModelUnitTests(helper.CPWebCase):
 
 class ModelSandboxUnitTest(helper.CPWebCase):
     """
-    Contains Tests for Model which can currently be only run under sandbox conditions due to Paypal Partner problems
+    Contains Unit Tests for Model which can currently be only run under sandbox conditions due to Paypal Partner problems
     """
+
     def test_stub_init_sandbox(self):
         """
         Checks if Paypal Stub is properly initialized
@@ -84,23 +85,28 @@ class ModelSandboxUnitTest(helper.CPWebCase):
         self.assertEqual(stub._token, token)
         self.assertEqual(stub.token_url, "https://api.sandbox.paypal.com/v1/oauth2/token")
         self.assertEqual(stub._extra_info, {"Authorization": ""})
-        self.assertEqual(stub._client_id, "AcMTMpvdMv1KMcweEIO_-KXrs4Y7AkHduqkf6r6u_e6-juZ1ZUxiP3QZIGp99zWba09_2AcihuENUgAR")
+        self.assertEqual(stub._client_id,
+                         "AcMTMpvdMv1KMcweEIO_-KXrs4Y7AkHduqkf6r6u_e6-juZ1ZUxiP3QZIGp99zWba09_2AcihuENUgAR")
         # TODO: change _client_id assert once live
-        self.assertEqual(stub._client_secret, "EIJGGRdItg83VE3mh0FIJ-9mR_Jd7ak4adK29VlZ4ygVETofhfr1PGG6afutUyoUj2rS7D6m69_gBrGG")
+        self.assertEqual(stub._client_secret,
+                         "EIJGGRdItg83VE3mh0FIJ-9mR_Jd7ak4adK29VlZ4ygVETofhfr1PGG6afutUyoUj2rS7D6m69_gBrGG")
         # TODO: change client secret assert once live
 
-    def test__getAuthorization(self):
+
+    def test_stub__getAuthorization(self):
+        """
+        Check if _getAuthorization returns correct token format
+        """
         with run_server():
             stub = ps()
-            listener = pcl()
-            # TODO: inject auth_code
-
-            token = ps._getAuthorization(ps)
-            self.assert_('access_token' in token)
-            self.assert_('token_type' in token)
-            self.assert_('expires_in' in token)
-            self.assert_('refresh_token' in token)
-            self.assert_('scope' in token)
+            auth_code = "C21AAFz4MLiqcHFKYwgvdPf2FGNa1YhyUVF8SegZHYf7IPSCygY6pLZFFtO3u9Bh195eHyT-UJRzmajzVjQC0629dUHe36jgA"
+            requests.get("http://localhost:4710/piggypal-listens", params={"code": auth_code, "scope": "openid"})
+            token = stub._getAuthorization()
+            self.assertTrue("access_token" in token)
+            self.assertTrue("token_type" in token)
+            self.assertTrue("expires_in" in token)
+            #self.assertTrue("refresh_token" in token)
+            self.assertTrue("scope" in token)
 
     def test_stub_get_incorrect_date_handling(self):
         """
@@ -117,24 +123,38 @@ class ModelSandboxUnitTest(helper.CPWebCase):
     def test_stub_get_correct_date_handling(self):
         """
         Check if proper Date Format will be accepted
+
+        A valid auth_code is needed here. It is possible that the example auth_code will expire in the near future an has to be replaced.
         """
         with run_server():
-            stub = ps()
             try:
-                # TODO: inject auth_code
-                stub.GET("2019-12-12T12:00:00.00Z", "2020-01-10T12:00:00.00Z")
+                auth_code = "C21AAFz4MLiqcHFKYwgvdPf2FGNa1YhyUVF8SegZHYf7IPSCygY6pLZFFtO3u9Bh195eHyT-UJRzmajzVjQC0629dUHe36jgA"
+
+                requests.get("http://localhost:4710/piggypal-listens", params={"code": auth_code, "scope": "openid"})
+                requests.get("http://localhost:4710/piggypal",
+                             params={"start_date": "2019-12-12T12:00:00.00Z", "end_date": "2020-01-10T12:00:00.00Z"})
+
             except AttributeError:
                 self.assertTrue(False)
             else:
                 self.assertTrue(True)
 
+class ModelSandboxIntegrationTest(helper.CPWebCase):
+    """
+    Contains Unit Tests for Model which can currently be only run under sandbox conditions due to Paypal Partner problems
+    """
     def test_stub_get_response(self):
         """
         Check if response from Paypal contains Transaction details
+
+        A valid auth_code is needed here. It is possible that the example auth_code will expire in the near future an has to be replaced.
         """
-        # TODO inject auth_code
-        stub = ps()
-        response = stub.GET("2019-12-12T12:00:00.00Z", "2020-01-10T12:00:00.00Z")
-        self.assert_("transaction_details" in response)
+        with run_server():
+            auth_code = "C21AAFz4MLiqcHFKYwgvdPf2FGNa1YhyUVF8SegZHYf7IPSCygY6pLZFFtO3u9Bh195eHyT-UJRzmajzVjQC0629dUHe36jgA"
+
+            requests.get("http://localhost:4710/piggypal-listens", params={"code": auth_code, "scope": "openid"})
+            response = requests.get("http://localhost:4710/piggypal", params={"start_date": "2019-12-12T12:00:00.00Z", "end_date": "2020-01-10T12:00:00.00Z"})
+
+            self.assertTrue("transaction_details" in response.text)
 
 

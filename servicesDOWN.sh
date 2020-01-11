ce57092b992c662b079e418dfe1f2e7894035d35
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2020-01-12 00:18:04 +0100

    Service up and down + service discovery

---
+++ servicesDOWN.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+aws2 ecs update-service --service config --desired-count 0 --cluster PiggyCluster
+
+aws2 ecs update-service --service account-service --desired-count 0 --cluster PiggyCluster
+
+aws2 ecs update-service --service registry --desired-count 0 --cluster PiggyCluster
+
+aws2 ecs update-service --service gateway --desired-count 0 --cluster PiggyCluster
+
+aws2 ecs update-service --service account-mongodb --desired-count 0 --cluster PiggyCluster
+
+aws2 ecs update-service --service auth-service --desired-count 0 --cluster PiggyCluster
+
+aws2 ecs update-service --service auth-mongodb --desired-count 0 --cluster PiggyCluster

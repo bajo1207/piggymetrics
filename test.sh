af5051f0b74aa1909b42d9d5cce65ed034e4bc2a
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2020-01-12 16:05:25 +0100

    Tests

--- test.sh
+++ test.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-if [ "$GREPPAL" == "piggypal" ]
-then
-    echo "Starting piggypal tests"
-    pip3 install pytest, requests, requests-oauthlib, cherrypy, oauthlib, re, contextlib
-    cd ./piggypal/src/main
-    python -m pytest
-fi
 
-if [ "$DIFFTREE" != "piggypal" ]
-then
-    echo "Starting piggymetrics tests"
-    mvn test -B
-fi
+echo "Starting piggypal tests"
+pip3 install pytest, requests, requests-oauthlib, cherrypy, oauthlib, re, contextlib
+cd ./piggypal/src/main
+python -m pytest
+
+
